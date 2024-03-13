3bed7a043eb1e40722747b3b1b4cd0cb5eee44bd
Pandanweavers <162727703+Pandanweavers@users.noreply.github.com>
2024-03-13 23:05:30 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+  <title>Pandan Weavers</title>
+  <link rel="stylesheet" href="../styles/css/marketing_page.css"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+  crossorigin="anonymous" 
+  referrerpolicy="no-referrer" />
+</head>
+<body>
+  <header>
+    <!-- NAVBAR -->
+    <nav class="navbar">
+      <img src="../styles/img/logo.png" class="logo">
+      <h3 class="title">PANDAN&nbspWEAVERS</h3>
+      <ul class="menu-links">
+        <a href="#home" class="active">Home</a>
+        <a href="#about">About</a>
+        <a href="#products">Products</a>
+        <a href="#membership">Membership</a>
+      </ul>
+    </nav>
+  </header>
+  
+    <!-- HOME PAGE -->
+  <section id="home" class="hero-section">
+    <div class="content-home">
+      <h1>PANDAN</h1>
+      <h1>WEAVERS</h1>
+      <button class="ba-button">BOOK APPOINTMENT</button>
+    </div>
+    <div class="book-appointment-popup">
+    <h1>BOOK APPOINTMENT</h1>
+
+      <div class="ba-close-btn">&times;</div>
+
+      <div class="book-appointment-form">
+
+            <div class="enter-name">
+                <div class="form-element">
+                    <label for="firstname">Firstname</label>
+                    <input type="text" id="firstname" name="firstname" required><br>
+                </div>
+
+                <div class="form-element">
+                    <label for="middlename">Middlename</label>
+                    <input type="text" id="middlename" name="middlename" required><br>
+                </div>
+
+                <div class="form-element">
+                    <label for="lastname">Lastname</label>
+                    <input type="text" id="lastname" name="lastname" required><br>
+                </div>
+            </div>
+
+            <div class="enter-contactno">
+                <div class="form-element">
+                    <label for="contactno">Contact Number</label>
+                    <input type="tel" id="contactno" name="contactno" required><br>
+                </div>
+            </div>
+
+        <h5>What date and time work best for you?</h5>
+        <div class="date-time">
+          <div class="form-element">
+              <label for="ba-date">Select date:</label>
+              <input type="date" id="appointmentDate" name="appointmentDate" required><br>
+          </div>
+
+          <div class="form-element">
+            <label for="ba-time">Select time:</label>
+            <input type="time" id="appointmentTime" name="appointmentTime" required><br>
+          </div>
+        </div>
+          
+
+        <h5>Any other specific date and time, if the above selection is not suitable.</h5>
+        <div class="date-time">
+          <div class="form-element">
+            <label for="ba-date">Select other date:</label>
+                <input type="date" id="appointmentDate" name="appointmentDate" required><br>
+          </div>
+
+          <div class="form-element">
+            <label for="ba-time">Select other time:</label>
+                <input type="time" id="appointmentTime" name="appointmentTime" required><br>
+          </div>
+        </div>
+
+        <div class="button-submit">
+            <div class="form-element">
+              <button type="submit">Submit</button>
+            </div>
+        </div>
+  
+      </div>
+
+    </div>
+  </section>
+  
+    <!-- ABOUT PAGE -->
+  <section id="about" class="hero-section">
+    <div class="content-about-1">
+      <h1>LUISIANA</h1>
+        <div class="about-infos-1">
+          <div class="about-img-1">
+            <img src="../styles/img/about-img-1.png">
+          </div>
+          <div class="about-infos-1">
+            <ul>
+            <p>
+              During the 17th century, there was an area of land in Laguna known as Terreno de Nasonog (Lupaín ng Nasonog in Tagalog). In 1678, Terreno de Nasúnog was divided into three parts: Nasúnog de Lucban, Nasonog de Cavinti, and Nasúnog de Majayjay. 
+            </p>
+            </br>
+            <p>
+              Nasonog de Majayjay later became the town of Luisiana. It was only on April 3, 1854, that ecclesiastical independence was granted to Nasonog by the Governor-General (the Marqués de Novaliches) with the corresponding approval from the Archbishop of Manila, having Don Marcos Bartolomé as its first interim parish priest. Because of the role of Don Luis Bernárdo, which was then regarded as the Father of Luisiana, and his wife Doña Ana, the town was named 'Luis y Ana', later changed to 'Luisiana'.
+            </p>
+            </br>
+            <p>
+              In 1903, the towns of Cavinti and Luisiana were combined, Pedro Villanueva of Cavinti was elected mayor and it was during his term that the Aglipay Church was founded in April 1904. The Romana family funded the construction of a couple of Protestant churches. However, in 1907, durin the leadership of Don Blas Oración, through the Civil Commission, Cavinti separated from Luisiana and became an independent town. 
+              </br></br>
+              In 1948, Visita de Luisiana gained civil independence from Majayjay.
+            </p>
+            </ul>
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <section id="about" class="hero-section">
+    <div class="content-about-2">
+      <h1>LUISIANA WEAVERS</h1>
+        <div class="about-infos-2">
+          <div class="about-infos-2">
+            <ul>
+            <h1 class="about-2-h1">VISION</h1>
+            <p>
+              Ang LUISIANA WEAVERS ORGANIZATION ay isang Samahan ng mga maglalala ng pandan sa bayan ng luisiana,na nagnanais na mapalawak at mapaunlad ang kabuhayanng mga mamamayan na naglalayong payabungin at mapagpatuloy ang industriya at kalakalan ng paglalala ng pandan tungo sa bagong henerasyon para sa pag unlad ng bayan ng mga luisianahin hindi lamang sa local kundi sa internasyonal.
+            </p>
+            </br>
+            <h1 class="about-2-h1">MISSION</h1>
+            <p>
+              Mapanatili ang paglalala bilang malaking bahagi ng ating kultura sa pamamagitan ng pagtuturo sa mga kabataan at pagkakaroon ng makabago at masining n paggawa. Gumawa ng mga dikalidad na Lalang produkto na yar isa pandan. Tiyakin na may susunod na henerasyon ng paglalala ng pandan sa luisiana. Palakasin ang organisasyon. lpakilala ang gawang pandan sa bansa at sa mundo. Magkaroon ng desenting kita ang mga myembro ng LWO at mamamayan ng luisiana.
+            </p>
+          </div>
+            </ul>
+            
+          <div class="about-img-2">
+            <img src="../styles/img/about-img-2.png">
+          </div>
+        </div>
+    </div>
+  </section>
+  
+    <!-- PRODUCTS PAGE -->
+  <section id="products" class="hero-section">
+  <div class="content-products-1">
+        <div class="products-infos-1">
+          
+        <div class="products-img-first">
+          <img src="../styles/img/weavers_bg.png" class="products-img-big">
+        </div>
+
+        <div class="products-weavers">
+        <h1>PANDAN PRODUCTS</h1>
+            <p>
+            "Weavers" are the individuals who perform the act of weaving, while "weaving" refers to the process itself. Weavers are skilled craftsmen or workers who operate looms to create woven fabrics through the process of weaving.
+            </p>
+        </div>
+          
+
+        </div>
+  </div>
+  </section>
+
+  <section id="products" class="hero-section">
+  <div class="content-products-2">
+        <div class="products-infos-2">
+          <div class="products-img-2">
+            <img class="left-pic" src="../styles/img/ladies_bag.png">
+          </div>
+          <div class="products-img-2">
+            <img class="middle-pic" src="../styles/img/customize_bag.png">
+          </div>
+          <div class="products-img-2">
+            <img class="right-pic" src="../styles/img/travel_bag.png">
+          </div>
+        </div>
+  </div>
+  </section>
+
+  <section id="" class="hero-section">
+    <ul>
+    <div class="content-products-3">
+        <div class="products-infos-3">
+          <ul>
+          <div class="products-img-3">
+            <img src="../styles/img/fruit_bag.png">
+          </div>
+          <div class="products-img-3">
+            <img src="../styles/img/wine_bag.png">
+          </div>
+          <div class="products-img-3">
+            <img src="../styles/img/pandan_hat.png">
+          </div>
+          </ul>
+          <ul>
+          <div class="products-img-3">
+            <img src="../styles/img/market_bag.png">
+          </div>
+          <div class="products-img-3">
+            <img src="../styles/img/shoulder_bag.png">
+          </div>
+          <div class="products-img-3">
+            <img src="../styles/img/bibingka_bag.png">
+          </div>
+          </ul>
+          <ul>
+          <div class="products-img-3">
+            <img src="../styles/img/bibingka_bag.png">
+          </div>
+          <div class="products-img-3">
+            <img src="../styles/img/mini_basket.png">
+          </div>
+          <div class="products-img-3">
+            <img src="../styles/img/manukan_bag.png">
+          </div>
+          </ul>
+        </div>
+  </section>
+
+  <section id="" class="hero-section">
+    <div class="content">
+    </div>
+  </section>
+  
+  <section id="membership" class="hero-section">
+    <div class="content-membership">
+    <h2 class="h2">MEMBERSHIP FORM</h2>
+        <div class="membership-form">
+            <div class="enter-name">
+                <div class="form-element">
+                    <label for="firstname">Firstname</label>
+                    <input type="text" id="firstname" name="firstname" required><br>
+                </div>
+
+                <div class="form-element">
+                    <label for="middlename">Middlename</label>
+                    <input type="text" id="middlename" name="middlename" required><br>
+                </div>
+
+                <div class="form-element">
+                    <label for="lastname">Lastname</label>
+                    <input type="text" id="lastname" name="lastname" required><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="membership-form">
+            <div class="enter-address">
+                <div class="form-element">
+                    <label for="address">Address</label>
+                    <input type="text" id="address" name="address" required><br>
+                </div>
+            </div>
+        </div>
+        
+        <div class="membership-form">
+            <div class="enter-birthday">
+                <div class="form-element">
+                    <label for="birthday">Birthday</label>
+                    <input type="date" id="birthday" name="birthday" required><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="membership-form">
+            <div class="enter-contactno">
+                <div class="form-element">
+                    <label for="contactno">Contact Number</label>
+                    <input type="tel" id="contactno" name="contactno" required><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="membership-form">
+            <div class="enter-nilala">
+                <div class="form-element">
+                    <label for="nilala">Klase ng nilalala</label>
+                    <select id="nilala" name="nilala"></select>
+        </div>
+
+        <div class="membership-form">
+            <div class="enter-registration">
+                <div class="form-element">
+                    <label for="contactno">Date of Registration</label>
+                    <input type="date" id="contactno" name="contactno" required><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="membership-form">
+            <div class="membership-submit">
+              <div class="form-element">
+                <button type="submit">Submit</button>
+              </div>
+            </div>
+        </div>   
+    </div>
+  </section>
+
+
+  <footer class="footer">
+        <div class="container">
+            <div class="icons">
+              <a href="https://www.facebook.com/profile.php?id=61555260497004"><i class="fa-brands fa-facebook"></i></a>
+              <h6></br>Luisiana Weavers</h6>
+              <a href=""><i class="fas fa-map-marker-alt"></i></a>
+              <h6></br>Estrellado st. Zone VI Luisiana, Laguna</h6>
+            </div>
+            <div class="icons">
+            </div>
+            <p class="footer-text">&copy; 2024 PANDAN WEAVERS. All rights reserved.</p>
+        </div>
+    </footer>
+    
+  <script src="../styles/js/marketing_page.js"></script>
+</body>
+</html>
